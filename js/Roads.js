3edf657a9201ba2f9466d4b3eb6b1554bb5651a8
Andrew Stephens <boss@andrewanimation.biz>
2014-10-25 03:33:45 -0400

    Roads dot Nodes

--- js/Roads.js
+++ js/Roads.js
@@ -9,17 +9,73 @@ var Roads = {
 			type: "GET",
 			dataType: "xml",
 			success: function(xml) {
-				console.log(xml);
+				Roads.parseXml(xml);
+				// TODO Send Message to Interface
 			},
 			error: function(e) {
 				alert('Could Not Access API');
+				// TODO Send Message to Interface
 			}
 		})
 
 	},
 
-	getIntersections: function() {
+	nodes: [],
+
+	parseXml: function(xml) {
+
+		// Maps Crazy Long Open Data IDs to Short Custom IDs
+		var usedIDs = [];
+		var nextId = 0;
+		var nodeToUniqueId = function(bigId) {
+			ourId = usedIDs.indexOf(bigId);
+			if (ourId == -1) {
+				usedIDs[nextId] = bigId;
+				nextId++;
+				return nextId-1;
+			} else {
+				return ourId;
+			}
+		}
+
+		$(xml).find('node').each(function() {
+			var node = $(this);
+
+			// New Node Object to Store
+			var nodeData = {
+				id: 0,
+				neighbors: []
+			};
+
+			// Set Id Using Custom Mapping Function
+			nodeData.id = nodeToUniqueId(node.attr('id'));
+
+			// Find Neighbors
+			$(xml).find('way').each(function() {
+				var way = $(this);
+
+				ndRefs = way.find('nd');
+				for (var i = 0; i < ndRefs.length; i++) {
+
+					if ($(ndRefs[i]).attr('ref') == node.attr('id')) {
+						if (i > 0) {
+							nodeData.neighbors.push(nodeToUniqueId($(ndRefs[i-1]).attr('ref')));
+						}
+						if (i < ndRefs.length-1) {
+							nodeData.neighbors.push(nodeToUniqueId($(ndRefs[i+1]).attr('ref')));
+						}
+					}
+
+				}
+
+			})
+
+			Roads.nodes.push(nodeData);
+
+		})
 
 	}
+
+
 
 }
