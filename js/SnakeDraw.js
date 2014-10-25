f4175050c361f41232f70fbf9c74101a4993dd68
Andrew Stephens <boss@andrewanimation.biz>
2014-10-25 09:48:03 -0400

    Andrew Learns How to Build a Queue

--- js/SnakeDraw.js
+++ js/SnakeDraw.js
@@ -4,6 +4,12 @@ var SnakeDraw = {
 
 	drawNewHead: function(lat, lng, hasGrown) {
 
+		// If Snake Has Not Grown, Remove Last Point
+		// Don't Be an Idiot and Put the New Element On First
+		if (!hasGrown && this.markers.length > 0) {
+			this.markers.pop().setMap(null)
+		}
+
 		var coord = new google.maps.LatLng(lat, lng);
 
 		var marker =  new google.maps.Marker({
@@ -11,11 +17,6 @@ var SnakeDraw = {
 			map: map,
 		});
 		this.markers.push(marker);
-
-		// If Snake Has Not Grown, Remove Last Point
-		if (!hasGrown && this.markers.length > 0) {
-			this.markers.pop().setMap(null)
-		}
 
 	}
 
